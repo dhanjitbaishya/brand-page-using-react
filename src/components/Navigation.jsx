696405e5a4eb28191af129e2472ba0b6cbb2cb7a
dhanjitbaishya <djbaishya007@gmail.com>
2024-12-08 22:18:53 +0530

    Initial commit for React brand page

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,16 @@
+const Navigation = () =>{
+    return <nav className="container">
+    <div className="logo">
+      <img src="/images/brand_logo.png" alt="logo"></img>
+    </div>
+  <ul>
+    <li href="#">Menu</li>
+    <li href="#">Location</li>
+    <li href="#">Contact</li>
+    <li href="#">About Us</li>
+  </ul>
+  <button>login</button>
+  </nav>
+};
+
+export default Navigation;
