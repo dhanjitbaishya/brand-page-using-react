e18b1fc9f65157966374fa61e1cf689e98ba65f2
dhanjitbaishya <djbaishya007@gmail.com>
2024-12-09 20:58:42 +0530

    fix errors

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
-  <BrowserRouter basename="/brand-page-using-react">
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
+
